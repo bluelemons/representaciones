1d18596e1a489ab6733f82d47b7be552b54ed09c
El_Hoy <eloyesp@gmail.com>
2011-02-18 19:10:07 -0300

    ADD: Inflecciones en castellano.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,15 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+# This change inflection rules to spanish ones.
+
+ActiveSupport::Inflector.inflections.clear
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural /([aeiou])([A-Z]|_|$)/, '\1s\2'
+  inflect.plural /([rlnd])([A-Z]|_|$)/, '\1es\2'
+  inflect.singular /([aeiou])s([A-Z]|_|$)/, '\1\2'
+  inflect.singular /([rlnd])es([A-Z]|_|$)/, '\1\2'
+  inflect.uncountable %w( dinero )
+end
+
