1a3b4bfd079c50c0aabdb3c04633df18c38115a8
El_Hoy <eloyesp@gmail.com>
2011-02-28 18:44:47 -0300

    corrijo los specs de modelo

--- lib/generators/rspec/model/templates/model_spec.rb
+++ lib/generators/rspec/model/templates/model_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe <%= class_name %> do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #asociaciones
+  <% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
+  it { shuld belong_to :<%= attribute.name %> }
+  <% end -%>
+  pending "add some more examples to (or delete) #{__FILE__}"
 end
+
