9edf91b6094a7396ad5c82fe529ede52a8180dae
El_Hoy <eloyesp@gmail.com>
2011-03-15 20:03:50 -0300

    corrijo el spec de deposito   Espero que sirva como modelo, porque fue una lucha

--- spec/views/movimientos/_deposito.haml_spec.rb
+++ spec/views/movimientos/_deposito.haml_spec.rb
@@ -2,15 +2,23 @@ require 'spec_helper'
 
 describe "movimientos/_deposito.haml" do
 
-  #before(:each) do
-  #  movimiento = stub_model(Movimiento).as_new_record
-  #  movimiento.stub(:monto) { stub_model(Monto).as_new }
-  #  assign(:movimiento, movimiento )
-  movimiento = Movimiento.new
-  movimiento.build_monto
+  before(:each) do
+    movimiento = stub_model(Movimiento).as_new_record
+    movimiento.stub(:monto) { stub_model(Monto).as_new_record }
+    assign(:movimiento, movimiento )
+  end
+# Finished in 0.43267 seconds
+
+#  before(:each) do
+#   movimiento = Movimiento.new
+#   movimiento.build_monto
+#   assign(:movimiento, movimiento )
+#  end
+# Finished in 0.54722 seconds
+
 
   it "renders new deposito form" do
-    render :partial =>"movimientos/deposito",:locals=>{:@movimiento=>movimiento}
+    render "movimientos/deposito"
     rendered.should have_selector("form", :action => movimientos_path, :method => "post" ) do |form|
       form.should have_selector("input#movimiento_fecha", :name => "movimiento[fecha]")
       form.should have_selector("select#movimiento_entidad_id", :name => "movimiento[entidad_id]")
