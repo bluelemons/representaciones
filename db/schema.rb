62d4cb36293078821018c560f4fafd3d7ca0cd39
Oldani Pablo <magicche@hotmail.com>
2011-03-18 12:31:03 -0300

    general fix

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,33 @@ ActiveRecord::Schema.define(:version => 20110204130234) do
     t.datetime "updated_at"
   end
 
+  create_table "deposito_versions", :force => true do |t|
+    t.integer  "deposito_id"
+    t.integer  "version"
+    t.integer  "tpago_id"
+    t.integer  "numero"
+    t.integer  "entidad_id"
+    t.integer  "movimiento_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "hidden",        :default => false
+    t.integer  "user_id"
+  end
+
+  add_index "deposito_versions", ["deposito_id"], :name => "index_deposito_versions_on_deposito_id"
+
+  create_table "depositos", :force => true do |t|
+    t.integer  "tpago_id"
+    t.integer  "numero"
+    t.integer  "entidad_id"
+    t.integer  "movimiento_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "hidden",        :default => false
+    t.integer  "user_id"
+    t.integer  "version"
+  end
+
   create_table "entidad_versions", :force => true do |t|
     t.integer  "entidad_id"
     t.integer  "version"
@@ -55,11 +82,11 @@ ActiveRecord::Schema.define(:version => 20110204130234) do
     t.string   "web"
     t.integer  "localidad_id"
     t.string   "name"
-    t.integer  "tentidad_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "hidden",       :default => false
+    t.boolean  "hidden",         :default => false
     t.integer  "user_id"
+    t.string   "versioned_type"
   end
 
   add_index "entidad_versions", ["entidad_id"], :name => "index_entidad_versions_on_entidad_id"
@@ -73,12 +100,19 @@ ActiveRecord::Schema.define(:version => 20110204130234) do
     t.string   "web"
     t.integer  "localidad_id"
     t.string   "name"
-    t.integer  "tentidad_id"
+    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "hidden",       :default => false
     t.integer  "user_id"
     t.integer  "version"
+  end
+
+  create_table "localidads", :force => true do |t|
+    t.string   "name"
+    t.integer  "departamento_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "moneda_versions", :force => true do |t|
