dfc40f4cbeeb5d97d4ef38239c9e962f1b82f9a1
Oldani Pablo <magicche@hotmail.com>
2011-03-21 00:06:10 -0300

    creando opcion para generar el deposito junto con el pago

--- app/controllers/pagos_controller.rb
+++ app/controllers/pagos_controller.rb
@@ -12,9 +12,23 @@ class PagosController < InheritedResources::Base
   def create
     @pago = Pago.new(params[:pago])
     @pago.user = current_user
+    #genero deposito
+
+    #si uso la opcion de depositar
+    if !(params[:use_saldo])
+      deposito = Deposito.new(params[:pago])
+      deposito.save
+      flash[:notice] = "#{deposito.errors}"
+      @pago.saldo = deposito.entidad.get_saldo(deposito.monto.moneda.id)
+    end
+    #debe tomar el saldo en el que deposite.
+
     if @pago.save
       flash[:notice] = "Por fin pagaron"
       redirect_to :action => 'new', :format =>'js'
+
+
+
     else
       @pago.build_monto
       @search = Reserva.baja.search(:agencia_id_eq=>0)
