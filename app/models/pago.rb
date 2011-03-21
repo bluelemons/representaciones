dbe4f2b8fddc2059cd564c0f6bba2d1b9066adc1
Oldani Pablo <magicche@hotmail.com>
2011-03-21 11:31:39 -0300

    metodo convertir

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -14,13 +14,20 @@ class Pago < Movimiento
   after_save :convertir
 
   def pago_minimo
-    #el pago no puede ser superior a la dueda.
+    # el pago no puede ser superior a la dueda.
   end
 
   def conventir
-    #se fija si el saldo del cual se va a realizar la extracion es en la misma
-    #moneda que la reservar.
-    #si no, genera el cambio.
+    # se fija si el saldo del cual se va a realizar la extracion es en la misma
+    # moneda que la reservar.
+    # si no, genera el cambio.
+    # esta opcion solo ocurre cuando se selecciona el saldo y despues
+    # de que se retiró de este.
+
+    if (reserva.moneda != monto.moneda)
+    #  monto.change(moneda_id)
+    #  monto.save
+    end
   end
 
   def depositar
