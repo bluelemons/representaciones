dfe0650e877e8ae16e38f7211ea3d8195088e492
Oldani Pablo <magicche@hotmail.com>
2011-03-28 18:52:30 -0300

    modificando el header de los reportes

--- app/reports/layout.rb
+++ app/reports/layout.rb
@@ -2,18 +2,22 @@ module Layout
   def header(title)
     repeat :all do
       font_size 10 do
-        draw_text "titulo", :at => [0,720.10]
-        draw_text "Listado de #{title}", :at => [0,710]
+        fill_color 'ea7b09'
+        draw_text "#{title}", :at => [0,710]
         stroke do
           line bounds.bottom_left, bounds.bottom_right
         end
+        image("#{RAILS_ROOT}/public/images/Sol_Lineas_Aereas.jpg",:at => [430,730],:scale=>0.35)
+        fill_color '000000'
+
       end
 
       font_size 6 do
         draw_text "Impreso: #{Date.today}",:at => [0,5]
-        draw_text "Litado de #{title}",:at => [100,5]
+        draw_text "#{title}",:at => [100,5]
         number_pages "<page> de <total>", [bounds.right - 50, 5]
-        draw_text "power by Blue Lemons",:at => [bounds.right - 150, 5]
+        image("#{RAILS_ROOT}/public/images/blue-lemons.png",:at => [bounds.right - 150, 13],:scale=>0.30)
+
       end
     end
   end
