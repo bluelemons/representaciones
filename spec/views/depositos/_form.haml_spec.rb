3f70543343f6d433de2b8a23ec6095423e80a7f9
Eloy <eloyesp@gmail.com>
2011-05-18 18:35:46 -0300

    Corrijo los specs de vistas.

--- spec/views/depositos/_form.haml_spec.rb
+++ spec/views/depositos/_form.haml_spec.rb
@@ -12,9 +12,9 @@ describe "depositos/_form.haml" do
     rendered.should have_selector("form", :action => depositos_path, :method => "post" ) do |form|
       form.should have_selector("input#deposito_fecha", :name => "deposito[fecha]")
       form.should have_selector("select#deposito_entidad_id", :name => "deposito[entidad_id]")
-      form.should have_selector("input#deposito_monto_attributes_valor", :name => "deposito[monto_attributes][valor]")
+      form.should have_selector("input#deposito_monto_fields_monto", :name => "deposito[monto_fields][monto]")
       form.should have_selector("select#deposito_tdeposito_id", :name => "deposito[tdeposito_id]")
-      form.should have_selector("select#deposito_monto_attributes_moneda_id", :name => "deposito[monto_attributes][moneda_id]")
+      form.should have_selector("select#deposito_monto_fields_monto_currency", :name => "deposito[monto_fields][monto_currency]")
     end
   end
 end
--- spec/views/pagos/_form.haml_spec.rb
+++ spec/views/pagos/_form.haml_spec.rb
@@ -4,22 +4,26 @@ describe "pagos/_form.haml" do
 
   before(:each) do
     pago = stub_model(Pago).as_new_record
+    search = Reserva.search
     assign(:pago, pago)
+    assign(:search, search)
   end
 
   it "renders new pago form" do
-    stub_template "reservas/qsearch.haml" => "<div class='qs_search'></div>"
+# TODO Habría que poner qsearch (el formulario de busqueda) en un partial
+# dejo el código comentado.
+#    stub_template "reservas/_qsearch.haml" => "<div class='qs_search'>busqueda</div>"
+    stub_template "reservas/_reservas.haml" => "<div class='reservas'>listado de Reservas</div>"
     render "pagos/form"
     rendered.should have_selector("form[data-remote]", :action => pagos_path, :method => "post") do |form|
       form.should have_selector("input#pago_fecha.datepicker", :name => "pago[fecha]")
       form.should have_selector("input#pago_entidad_id", :name => "pago[entidad_id]", :type=>"hidden")
       form.should have_selector("input#pago_reserva_id", :name => "pago[reserva_id]")
-      form.should have_selector("input#pago_monto_attributes_valor", :name => "pago[monto_attributes][valor]")
-      form.should have_selector("select#pago_tdeposito_id", :name => "pago[tdeposito_id]")
-      form.should have_selector("select#pago_monto_attributes_moneda_id", :name => "pago[monto_attributes][moneda_id]")
+      form.should have_selector("input#pago_monto_fields_monto", :name => "pago[monto_fields][monto]")
+      form.should have_selector("select#pago_monto_fields_monto_currency", :name => "pago[monto_fields][monto_currency]")
     end
-    rendered.should have_selector("div.qs_search", :count=>1)
-    rendered.should have_selector("div#cuentas", :count=>1)
+    rendered.should have_selector("div.reservas", :count=>1)
+#    rendered.should have_selector("div.qs_search", :count=>1)
   end
 end
 
