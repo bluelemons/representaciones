a75ca55ff34ef2958f4d241334a9a25b88d1171c
El_Hoy <eloyesp@gmail.com>
2011-05-20 22:30:23 -0300

    Factories Agencia y Operadora

--- spec/factories/agencia.rb
+++ spec/factories/agencia.rb
@@ -1,11 +1,11 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :agency do |f|
-  f.name "Turismo Albear"
-  f.calle "Albear 5234"
-  f.telefono "456-8791"
-  f.legajo "186324"
-  f.email "turismo@albear.com.ar"
-  f.web "www.myweb.com"
+  f.name      { Forgery(:name).company_name }
+  f.calle     "Albear 5234"
+  f.telefono  "456-8791"
+  f.legajo    "186324"
+  f.email     "turismo@albear.com.ar"
+  f.web       "www.myweb.com"
 end
 
--- spec/factories/operadora.rb
+++ spec/factories/operadora.rb
@@ -1,13 +1,13 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :operadora do |f|
-  f.calle "MyString"
+  f.name      { Forgery(:name).company_name }
+  f.calle     "MyString"
   f.cuit 1
   f.telefono "MyString"
   f.legajo "MyString"
   f.email "email@email.com"
   f.web "www.myweb.com"
   f.localidad_id 4
-  f.name "MyString"
 end
 
