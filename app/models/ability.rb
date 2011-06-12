06843b502eb7a4d43e011afe895daa3a8c2a5642
Oldani Pablo <magicche@hotmail.com>
2011-06-12 18:35:35 -0300

    cambio en modelo de ability para que se entienda un poco mas.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,41 @@
-class Ability  
-  include CanCan::Ability  
-  
-  def initialize(user)  
-    user ||= User.new # Guest user  
-    if (user.role?("Admin")) 
-      can :manage, :all  
-    else  
-      can :read, :all  
-      can :create, Post  
-      can :update, Post do |comment|  
-        comment.try(:user) == user  
-      end  
-    end  
-  end 
+class Ability
+  include CanCan::Ability
 
+  def initialize(user)
+    user ||= User.new # Guest user
+
+    can :read, :all # todos pueden ver todo.
+
+    user.roles.each do |r|
+      self.send(r.name.downcase)
+    end
+
+  end
+
+  #el usuario admin tiene todos los privilegios
+  def admin
+    can :manage, :all
+  end
+
+  #privilegio sobre las reservas.
+  def reservas
+    can :create, Reserva
+    can :update, Reserva
+  end
+
+  #puede dar altas y bajas de pagos depostios y cambios
+  def pagos
+    can :create, Movmiento,Pago,Deposito,Cambio
+    can :desrtoy, Movmiento,Pago,Deposito,Cambio
+  end
+
+  def tablas
+    can :manage, Operadora
+    can :manage, Agency
+    can :manage, Programa
+    can :manage, Thabitacion
+    can :manage, Cotizacion
+    can :manage, Pasajero
+  end
 end
+
