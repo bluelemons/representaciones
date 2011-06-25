a0d8fd01ec7265b9709d9a477e09f178ddf16c9c
Oldani Pablo <magicche@hotmail.com>
2011-06-25 00:54:10 -0300

    saco las lineas.

--- app/reports/layout.rb
+++ app/reports/layout.rb
@@ -4,19 +4,19 @@ module Layout
       font_size 10 do
         fill_color 'ea7b09'
         draw_text "#{title}", :at => [0,710]
-        stroke do
-          line bounds.bottom_left, bounds.bottom_right
-        end
+        #stroke do
+        #  line bounds.bottom_left, bounds.bottom_right
+        #end
         image("#{RAILS_ROOT}/public/images/Sol_Lineas_Aereas.jpg",:at => [430,730],:scale=>0.35)
         fill_color '000000'
 
       end
 
       font_size 6 do
-        draw_text "Impreso: #{Date.today}",:at => [0,5]
-        draw_text "#{title}",:at => [100,5]
-        number_pages "<page> de <total>", [bounds.right - 50, 5]
-        image("#{RAILS_ROOT}/public/images/blue-lemons.png",:at => [bounds.right - 150, 13],:scale=>0.30)
+        draw_text "Impreso: #{Date.today}",:at => [0,0]
+        draw_text "#{title}",:at => [100,0]
+        number_pages "<page> de <total>", [bounds.right - 50, 0]
+        image("#{RAILS_ROOT}/public/images/blue-lemons.png",:at => [bounds.right - 150, 8],:scale=>0.30)
 
       end
     end
