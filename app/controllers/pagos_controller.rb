a49dfec3cc7f3d70b222fe6e5f4ab34f190fec31
Oldani Pablo <magicche@hotmail.com>
2011-07-06 19:41:29 -0300

    agrego tab para que ser pueda modificar la observaiones de los pagos.

--- app/controllers/pagos_controller.rb
+++ app/controllers/pagos_controller.rb
@@ -10,6 +10,18 @@ class PagosController < InheritedResources::Base
     @reservas = @search.paginate :page => params[:page], :per_page =>10
   end
 
+  def update
+    @movimiento = Pago.find(params[:id])
+    @movimiento.user = current_user
+    if @movimiento.update_attributes(params[:pago])
+      flash[:notice] = "El pago a sido actualizado"
+      render 'movimientos/edit.js'
+    else
+      flash[:notice] = "El pago no se ha podido actualizar, intente nuevamente"
+      render 'movimientos/edit.js'
+    end
+  end
+
   def create
     @pago = Pago.new(params[:pago])
     @pago.user = current_user
