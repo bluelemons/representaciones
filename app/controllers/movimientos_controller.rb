9f983dd51ade7c29c0804ece458eace410652aaf
Eloy Espinaco <eloyesp@gmail.com>
2012-01-10 12:33:48 -0300

    Limpio el controller de movimientos y muestro solo los depositos.

--- app/controllers/movimientos_controller.rb
+++ app/controllers/movimientos_controller.rb
@@ -2,17 +2,18 @@ class MovimientosController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @search = Movimiento.baja.search((params[:search] if params[:search]))
-    @total = Movimiento.total(@search)
-    @movimientos = @search.paginate :page => params[:page], :per_page =>10
+    @search = Deposito.baja.search(params[:search])
+    @movimientos = @search.paginate(:page=>params[:page], :per_page=>10)
+    @movimientos = @movimientos.includes :entidad, :reserva => :pasajeros
 
-    @reservas_search = Reserva.baja.search(:agency_id_eq=>0)
+    @reservas_search = Reserva.baja.search()
     @reservas = @reservas_search.paginate :page => params[:page], :per_page =>10
 
     respond_to do |format|
       format.js
       format.html
       format.pdf do
+        @total = Movimiento.total(@search)
         output = MovimientoReport.new.to_pdf(@search,@total)
         send_data output, :filename => "index_report.pdf",
                           :type     => "application/pdf"
