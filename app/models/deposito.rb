fb2a0f11010ceaf21cf3990cae17c4993f90a344
Eloy Espinaco <eloyesp@gmail.com>
2012-01-16 02:58:02 -0300

    Validaciones en deposito -- closes #16

--- app/models/deposito.rb
+++ app/models/deposito.rb
@@ -10,6 +10,11 @@ class Deposito < Movimiento
   # Validaciones
   #
   validates_presence_of :tdeposito, :numero
+  validate :monto_final_en_moneda_de_la_reserva
+
+  validates :monto_final_cents, :presence => true
+  validates :monto_final_currency, :presence => true
+  validate  :monto_final_positivo
 
   #
   # Asociaciones
@@ -28,10 +33,22 @@ class Deposito < Movimiento
 
   private
 
+  def monto_final_positivo
+    errors.add(:monto_final, "debe ser positivo") if monto_final.cents <= 0
+  end
+
+
   def completar_monto
     if monto.zero? and monto_final.nonzero?
       self.monto = monto_final
     end
   end
 
+  def monto_final_en_moneda_de_la_reserva
+    if reserva and monto_final.nonzero?
+      errors.add(:base, "La moneda de la reserva y del deposito no coinciden, seleccione la reserva nuevamente") unless monto_final.currency == reserva.total.currency
+    end
+  end
+
 end
+
