c5993add82f60c403ed2f80a737a9dc164a03822
Eloy Espinaco <eloyesp@gmail.com>
2012-01-18 09:48:54 -0300

    Test sobre #17

--- test/unit/deposito_test.rb
+++ test/unit/deposito_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class DepositoTest < ActiveSupport::TestCase
 
+  def setup
+    @deposito = movimientos(:deposito_ibero)
+  end
+
   def test_format_monto
     deposito_sin_cambio_de_moneda = movimientos(:deposito_ibero)
     assert_equal "u$s 2.000,00", deposito_sin_cambio_de_moneda.format_monto
@@ -12,10 +16,14 @@ class DepositoTest < ActiveSupport::TestCase
   def test_validations
     d = Deposito.new
     assert d.invalid?, "Deposito valido sin datos"
-    d = movimientos(:deposito_vikingo)
-    assert d.valid?, "Deposito en fixture invalido"
-    d.monto_final = "500 ARS"
-    assert d.invalid?, "Deposito valido en una moneda distinta que la de la reserva"
+    assert @deposito.valid?, "Deposito en fixture invalido"
+    @deposito.monto_final = "500 ARS"
+    assert @deposito.invalid?, "Deposito valido en una moneda distinta que la de la reserva"
+  end
+
+  def test_validacion_de_reserva
+    @deposito.entidad = entidads(:litoraltur)
+    assert @deposito.invalid?, "Deposito valido, por una entidad que no corresponde a la reserva"
   end
 
 end
