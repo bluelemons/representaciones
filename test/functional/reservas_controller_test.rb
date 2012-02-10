c209a4370a4bdc009907e41724c2f0d658ba2b24
Eloy Espinaco <eloyesp@gmail.com>
2012-02-10 02:11:18 -0300

    agrego iva en los datos exportados como csv
    Incluyo tests.
    Agrego iva en un fixture de reserva.

--- test/functional/reservas_controller_test.rb
+++ test/functional/reservas_controller_test.rb
@@ -7,6 +7,18 @@ class ReservasControllerTest < ActionController::TestCase
 
     assert_response :success
     assert_match /csv/, response.content_type, "respond with csv"
+    headers, *data = CSV.parse(response.body, col_sep: ";" )
+    # Hash[[header, data].transpose]
+    required_headers = %w[ id agencia operadora salida referencia
+                           bruto seguro iva neto moneda ]
+    required_headers.each do |header|
+      assert_includes headers, header
+    end
+
+    required_data = %W[ 21,5 ]
+    required_data.each do |value|
+      assert_includes data.flatten, value
+    end
   end
 end
 
