6c0006d90c7fd8a2143d4536b494287952041957
Eloy <eloyesp@gmail.com>
2012-02-17 16:13:24 -0300

    complete tests.

--- test/unit/helpers/date_helper_test.rb
+++ test/unit/helpers/date_helper_test.rb
@@ -6,6 +6,14 @@ class DateHelperTest < ActionView::TestCase
       string = beauty_date(value)
       assert_match /^<time.*>.*<\/time>$/, string, "is not a time element"
       assert_match /class="[\w\s]*appendTimeago[\w\s]*"/, string, "hasn't the class appendTimeago"
+
+      /datetime="(?<datetime>.*)".*>(?<contents>.*)<\/time>/ =~ string
+      assert_nothing_raised do
+        DateTime.iso8601(datetime)
+      end
+
+      assert_match /\w{3} \d{2}\/\w{3}\/\d{4}/, contents, "la fecha esta mal escrita"
+      assert_match /\d{2}:\d{2}/, contents, "la hora no esta escrita" if value.acts_like?(:time)
       assert beauty_date(value).html_safe?, "the html is not marked as safe"
     end
   end
