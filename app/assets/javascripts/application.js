9e0252011329b94a7cb40946f30923309707d638
Eloy Espinaco <eloyesp@gmail.com>
2012-03-05 10:32:24 -0300

    Update: actualizo y pongo en vendor el jqGrid.
    Habilito el asset pipeline.
    El archivo de traducción en jqgrid tiene que ser llamado antes que los otros
    porque sinó no carga la tabla.
    Quito las fuentes de app/assets.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
 //= require jquery
-//= require jquery_ujs
 //= require jquery-ui
-//= require 'jquery.jqGrid.min'
-//= require 'menu'
+//= require jquery_ujs
+//= require jqGrid
+//= require jquery.freeow.min
+//= require menu
+//= require autocompletePersona
+//= require autocomplete
+//= require jq.reservas
+//= require jq.voucher
+//= require jquery.doublesubmit
+
+
+
 
 //request = $.ajax();
   //agrega un tab con
@@ -165,3 +186,4 @@ $(function() {
 
   skin();
   });
+
