9429a2e5f2b7c6a9a02728eeb4bbdb2966da889a
Eloy Espinaco <eloyesp@gmail.com>
2017-09-05 11:21:35 -0300

    Remove localidads references on the seeds

--- db/seeds/agencias/agencias.rb
+++ db/seeds/agencias/agencias.rb
@@ -5,20 +5,12 @@ Agency.delete_all
 # open need tables
 agencias = DBF::Table.new(File.join(Rails.root, 'db', 'seeds', 'agencias', "AGENCIA.DBF"))
 # cache need data
-Localidad.all
 Agency.all
 
 agencias.each do |a|
   # debug info
   puts a.attributes
-  if l = Localidad.where("name LIKE ?", a.localidad.gsub!(/\./,"")).try(:first)
-    localidad_id = l.id
-  else
-    localidad_id = nil
-  end
   puts Agency.create(:name => a.nombre, :calle => a.direccion,
                  :cuit => a.nro_cuit, :telefono => a.telefono,
-                 :email => a.e_mail, :web => a.pag_web,
-                 :localidad_id => localidad_id).inspect
+                 :email => a.e_mail, :web => a.pag_web).inspect
 end
-
