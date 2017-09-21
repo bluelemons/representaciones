157196631ab3d3752d13505e0fb85943330dd30b
Eloy Espinaco <eloyesp@gmail.com>
2017-09-21 11:05:00 -0300

    Pase form genera principalmente el listado de reservas receptoras del pase

--- app/models/pase_form.rb
+++ app/models/pase_form.rb
@@ -5,54 +5,35 @@ class PaseForm
   def initialize reserva
     @reserva = reserva
     @pase = Pase.new
-    asignar_reserva
+    @pase.origen_id = reserva.id
+    @pase.monto = saldo(reserva)
     self.origen_id = reserva.id
   end
 
   def posibles_reservas
     @posibles_reservas ||= Reserva
-      .where(agency_id: @reserva.agency_id, operadora_id: @reserva.operadora_id)
-      .map { |reserva| [reserva, reserva.id, data_for(reserva)] }
-  end
-
-  def reservas_con_saldo
-    @reservas_con_saldo ||= Reserva
-      .where(agency_id: @reserva.agency_id, operadora_id: @reserva.operadora_id)
-      .where('salida > ?', 6.months.ago)
+      .where(agency_id: @reserva.agency_id)
+      .where(operadora_id: @reserva.operadora_id)
+      .where('salida > ?', 10.months.ago)
       .includes(:depositos)
-      .select { |reserva| tiene_saldo? reserva }
-      .map { |reserva| [reserva, reserva.id, data_for(reserva)
-   ] }
-  end
-
-  def persisted?
-    false
+      .map { |reserva| [reserva, reserva.id, data_for(reserva)] }
   end
 
   private
 
-  def tiene_saldo? reserva
-    reserva.agencia_deuda.cents.negative? &&
-      reserva.operadora_deuda.cents.negative?
-  end
-
   def data_for reserva
     { 'data-currency': reserva.total_currency,
-      'data-saldo': saldo(reserva) }
+      'data-debt': debt(reserva) }
+  end
+
+  def debt reserva
+    [reserva.agencia_deuda,
+     reserva.operadora_deuda].max.to_d
   end
 
   def saldo reserva
     [reserva.agencia_deuda,
      reserva.operadora_deuda].max.abs.to_d
-  end
-
-  def asignar_reserva
-    if tiene_saldo? @reserva
-      pase.origen_id = @reserva.id
-      pase.monto = saldo(@reserva)
-    else
-      pase.destino = @reserva.id
-    end
   end
 
 end
