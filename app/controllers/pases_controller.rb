b973cca961e4fcbb261808306a1fff1baacc5cc5
Eloy Espinaco <eloyesp@gmail.com>
2017-09-25 09:35:19 -0300

    Quito basura del controlador

--- app/controllers/pases_controller.rb
+++ app/controllers/pases_controller.rb
@@ -16,39 +16,4 @@ class PasesController < ApplicationController
     @pase = Pase.new params[:id]
     render text: @pase.inspect
   end
-
-
-  def basura
-    origen = Reserva.find(params[:pase][:origen_id])
-    destino = Reserva.find(params[:pase][:destino_id])
-    pase_params = params[:pase]
-    @pase = Pase.new origen
-    Movimiento.transaction do
-      tipo = Tdeposito.where(name: 'Pase').first
-      #saco la plata de la reserva origen
-      agencia = origen.agencia
-      operadora = origen.operadora
-      monto = Money.new pase_params[:monto].to_d * 100, origen.total.currency
-      if ["agencia", "ambas"].include? pase_params[:cuentas_afectadas]
-        Deposito.create! fecha: Date.today, entidad: agencia, reserva: origen, monto: monto * -1,
-          monto_final: monto * -1,
-          tdeposito: tipo, numero: 12,
-          observaciones: "Pase a #{ destino } de #{ monto }"
-        Deposito.create! fecha: Date.today, entidad: agencia, reserva: destino, monto: monto,
-          monto_final: monto,
-          tdeposito: tipo, numero: 12,
-          observaciones: "Pase desde #{ origen } de #{ monto }"
-      end
-      if ["operadora", "ambas"].include? pase_params[:cuentas_afectadas]
-        Deposito.create! fecha: Date.today, entidad: operadora, reserva: origen, monto: monto * -1,
-          monto_final: monto * -1,
-          tdeposito: tipo, numero: 12,
-          observaciones: "Pase a #{ destino } de #{ monto }"
-        Deposito.create! fecha: Date.today, entidad: operadora, reserva: destino, monto: monto,
-          monto_final: monto,
-          tdeposito: tipo, numero: 12,
-          observaciones: "Pase desde #{ origen } de #{ monto }"
-      end
-    end
-  end
 end
