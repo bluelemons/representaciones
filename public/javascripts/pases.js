2796ce8ba06e504150d7b1224bcad2212d418f39
Eloy Espinaco <eloyesp@gmail.com>
2017-09-27 09:12:21 -0300

    Encapsulo el código para extraer funciones

--- public/javascripts/pases.js
+++ public/javascripts/pases.js
@@ -1,32 +1,32 @@
-$(document).on('change', '#nuevo_destino', function (e) {
-  const fieldset = document.querySelector('#destino')
-    .content.querySelector('fieldset')
-  const destinos = document.querySelector('.destinos')
-  const clone = document.importNode(fieldset, true)
+(function () {
+  $(document).on('change', '#nuevo_destino', function (e) {
+    const fieldset = document.querySelector('#destino')
+      .content.querySelector('fieldset')
+    const destinos = document.querySelector('.destinos')
+    const clone = document.importNode(fieldset, true)
 
-  // agrego el formulario de destino
-  const selected = this.selectedOptions[0]
-  document.sample_clone = clone
-  const remaining = document.querySelector('#remaining')
-  const current_value = Math.min(remaining.textContent, selected.dataset.debt)
+    // agrego el formulario de destino
+    const selected = this.selectedOptions[0]
+    const remaining = document.querySelector('#remaining')
+    const current_value = Math.min(remaining.textContent, selected.dataset.debt)
 
-  const template_data = {
-    name: selected.innerText,
-    id: selected.value,
-    value: current_value,
-    max: selected.dataset.debt,
-    currency: selected.dataset.currency }
+    const template_data = {
+      name: selected.innerText,
+      id: selected.value,
+      value: current_value,
+      max: selected.dataset.debt,
+      currency: selected.dataset.currency }
 
-  clone.innerHTML = clone.innerHTML.replace(/{{[^}]*}}/g, function (match) {
-    return template_data[match.slice(2, -2).trim()]
+    clone.innerHTML = clone.innerHTML.replace(/{{[^}]*}}/g, function (match) {
+      return template_data[match.slice(2, -2).trim()]
+    })
+    destinos.appendChild(clone);
+
+    // evitamos dos veces el mismo destino
+    selected.disabled = true
+    this.options[0].selected = true
+
+    // Actualizo saldo restante
+    remaining.innerText = (parseFloat(remaining.innerText) - parseFloat(current_value)).toFixed(2)
   })
-  destinos.appendChild(clone);
-
-  // evitamos dos veces el mismo destino
-  selected.disabled = true
-  this.options[0].selected = true
-  console.log(selected.selected)
-
-  // Actualizo saldo restante
-  remaining.innerText = (parseFloat(remaining.innerText) - parseFloat(current_value)).toFixed(2)
-})
+})()
